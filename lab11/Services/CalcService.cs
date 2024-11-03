865cc79fa37adcaee46bc24076ad7c0e63e3c591
NikitaVolkov777 <volkniknikvolk@gmail.com>
2024-11-03 14:05:55 +0700

    Add files via upload

---
+++ lab11/Services/CalcService.cs
@@ -0,0 +1,11 @@
+﻿namespace CalcService.Services
+{
+    public class CalcService1
+    {
+        public int Add(int a, int b) => a + b;
+        public int Subtract(int a, int b) => a - b;
+        public int Multiply(int a, int b) => a * b;
+        public string Divide(int a, int b) => b == 0 ? "!!Error!! Division by zero. Be careful" : (a / b).ToString();
+
+    }
+}
