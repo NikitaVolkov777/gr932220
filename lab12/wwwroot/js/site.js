de22c4ad1c22b1096da5e201fba1d82bea0ed1bc
NikitaVolkov777 <volkniknikvolk@gmail.com>
2024-11-09 14:04:48 +0700

    Add files via upload

---
+++ lab12/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿function calculate() {
+    const operand1 = document.getElementById("operand1").value;
+    const operator = document.getElementById("operator").value;
+    const operand2 = document.getElementById("operand2").value;
+
+    fetch('/ParsingModel/Calculate', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ operand1: parseInt(operand1), Operator: operator, operand2: parseInt(operand2) })
+    })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("result").innerHTML = `<p>${data.result} ЫЫЫЫ</p> `;
+            document.getElementById("calcForm").reset(); // Очищает форму после отправки
+        })
+        .catch(error => console.error('Error:', error));
+}
